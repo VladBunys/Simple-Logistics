fdcb453bd1c82187f2eb9d81ae9edb1d5ef0b0c1
VladBunys <52935929+VladBunys@users.noreply.github.com>
2021-11-14 18:12:12 +0000

    Update Source.cpp

--- LogisticsEstimate/LogisticsEstimate/Source.cpp
+++ LogisticsEstimate/LogisticsEstimate/Source.cpp
@@ -3,19 +3,9 @@
 /*
 			Logistics Calculator
 			--------------------
-The programm reads .csv file with 4 data columns.
-1st column should be the name of the city (or any name chosen to call a set of coordinates).
-2nd column is some demand variable, populaton of a city is used in the example.
-3rd column is latitude and 4th is longitude.
-The program uses Haversine formula to calculate the distance between two points on Earth.
-Also computes the sum of all distances from a point to all the other points.
-The programm can be used to find the best place to put a delivery hub excluding or including 
-the demand. 
-The .csv file must also have it's first row filled with column titles
 
 */
 /*            Assumptions
-			  -----------
 
 1) The lengh of the roads is equal to the great circle distance
 2) Each Place has a direct road to all other places
